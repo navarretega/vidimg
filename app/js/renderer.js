5df72707ae6919c021433b525b43c567444793e4
Alex Navarrete <anavarrete.ga@gmail.com>
2020-07-01 22:43:32 -0500

    Images Functionality - Convert Images to Video

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -24,8 +24,15 @@ const fpsEle = document.getElementById("fps");
 const videoNameEle = document.getElementById("video-name");
 const spinner = document.getElementById("spinner-container");
 
+const imagesFormEle = document.getElementById("img-form");
+const imagesDirEle = document.getElementById("images-dir");
+const imagesDirNameEle = document.getElementById("images-dir-name");
+const imagesFpsEle = document.getElementById("images-fps");
+const imgSpinner = document.getElementById("spinner-img-container");
+
 const outputBasePath = path.join(os.homedir(), "vidimg");
 let videoPath;
+let imagesPath;
 
 // ** Functions ** //
 
@@ -57,13 +64,13 @@ async function onChange() {
   // Verify ffprove
   const res = await execShellCommand("ffprobe -version");
   if (res["err"]) {
-    errorAlert(
+    showAlert(
       "You need to have FFMPEG/FFPROBE installed, and add them to your PATH!",
       "<a target='_blank' href='https://www.ffmpeg.org/download.html'>Click here to install</a>"
     );
   } else {
     // Replace Video Name
-    videoPath = '"' + video.files[0].path + '"';
+    videoPath = '"' + videoEle.files[0].path + '"';
     videoNameEle.innerText = path.basename(videoPath);
 
     // Get duration
@@ -111,12 +118,12 @@ async function onChange() {
   }
 }
 
-// Handle Submit Form Event
+// Handle Submit Form Event - Video
 async function onSubmit(e) {
   e.preventDefault();
   const res = await execShellCommand("ffmpeg -version");
   if (res["err"]) {
-    errorAlert(
+    showAlert(
       "error",
       "Oops...",
       "You need to have FFMPEG/FFPROBE installed, and add them to your PATH!",
@@ -136,8 +143,9 @@ async function onSubmit(e) {
     const width = widthEle.value;
     const height = heightEle.value;
     const fps = fpsEle.value;
+    const out = path.join(outputPath, "image_%05d.jpg");
 
-    const cmd = `ffmpeg -i ${videoPath} -ss ${start} -to ${end} -vf "fps=${fps}, scale=${width}:${height}" "${outputPath}\\image_%05d.jpg"`;
+    const cmd = `ffmpeg -i ${videoPath} -ss ${start} -to ${end} -vf "fps=${fps}, scale=${width}:${height}" "${out}"`;
     console.log(cmd);
 
     // Add animation
@@ -151,7 +159,7 @@ async function onSubmit(e) {
     // Verify result
     if (res["err"]) {
       console.log(res["err"]);
-      errorAlert("error", "Sorry!", "Something bad happen.");
+      showAlert("error", "Sorry!", "Something bad happen.");
     } else {
       showAlert(
         "success",
@@ -168,28 +176,99 @@ async function onSubmit(e) {
   }
 }
 
-// ** Event Listeners ** //
-
-// Video Input
-videoEle.addEventListener("change", onChange);
-
-// Handle Submit
-formEle.addEventListener("submit", (e) => onSubmit(e));
-
-// Tab Events
-videoTabEle.addEventListener("click", () => {
+// Handle Tab Switches - Video Tab
+function videoTabSwitch() {
   if (!videoContentEle.classList.contains("active")) {
     imageContentEle.classList.remove("active");
     imageTabEle.classList.remove("tl-active");
     videoContentEle.classList.add("active");
     videoTabEle.classList.add("tl-active");
   }
-});
-imageTabEle.addEventListener("click", () => {
+}
+
+// Handle Tab Switches - Image Tab
+function imageTabSwitch() {
   if (!imageContentEle.classList.contains("active")) {
     videoContentEle.classList.remove("active");
     videoTabEle.classList.remove("tl-active");
     imageContentEle.classList.add("active");
     imageTabEle.classList.add("tl-active");
   }
-});
+}
+
+// Set image name (input)
+function setImageInput() {
+  imagesPath = path.dirname(imagesDirEle.files[0].path);
+  imagesDirNameEle.innerText = imagesPath;
+  imagesDirNameEle.setAttribute("title", imagesPath);
+}
+
+// Handle Submit Form Event - Images
+async function onImageSubmit(e) {
+  e.preventDefault();
+  const res = await execShellCommand("ffmpeg -version");
+  if (res["err"]) {
+    showAlert(
+      "error",
+      "Oops...",
+      "You need to have FFMPEG/FFPROBE installed, and add them to your PATH!",
+      "<a target='_blank' href='https://www.ffmpeg.org/download.html'>Click here to install</a>"
+    );
+  } else {
+    // Create output dir
+    const currDat = moment().format("MMDDYYYYTHHmmss");
+    const outputPath = path.join(outputBasePath, currDat);
+    const out = path.join(outputPath, "video.mp4");
+    if (!fs.existsSync(outputPath)) {
+      fs.mkdirSync(outputPath);
+    }
+
+    // Prepare ffmpeg command with the given inputs
+    const cmd = `cat "${imagesPath}/"*.{png,jpg} | ffmpeg -f image2pipe -framerate ${imagesFpsEle.value} -i - "${out}"`;
+    console.log(cmd);
+
+    // Add animation
+    imagesFormEle.classList.remove("is-visible");
+    imgSpinner.classList.add("is-visible");
+
+    // Run ffmpeg command
+    const res = await execShellCommand(cmd);
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    // Verify result
+    if (res["err"]) {
+      console.log(res);
+      showAlert("error", "Sorry!", "Something bad happen.");
+    } else {
+      showAlert(
+        "success",
+        "Your video is ready!",
+        `You can find it here: ${out}`
+      );
+
+      // Reset animation
+      imgSpinner.classList.remove("is-visible");
+      imagesFormEle.classList.add("is-visible");
+
+      // shell.openPath(outputPath)
+    }
+  }
+}
+
+// ** Event Listeners ** //
+
+// Video Input
+videoEle.addEventListener("change", onChange);
+
+// Handle Video Submit
+formEle.addEventListener("submit", (e) => onSubmit(e));
+
+// Tab Events
+videoTabEle.addEventListener("click", videoTabSwitch);
+imageTabEle.addEventListener("click", imageTabSwitch);
+
+// Image Input
+imagesDirEle.addEventListener("change", setImageInput);
+
+// Handle Image Submit
+imagesFormEle.addEventListener("submit", (e) => onImageSubmit(e));
